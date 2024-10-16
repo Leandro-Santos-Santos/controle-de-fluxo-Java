63881ae96808a30dca0086f9e271bf0a39502503
Leandro-Santos-Santos <santosleandro1805@gmail.com>
2024-10-16 19:28:10 -0300

    do while controle de fluxo

---
+++ controle de fluxo/src/FormatadorCepExemplo.java
@@ -0,0 +1,25 @@
+public class FormatadorCepExemplo {
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("23765064");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            System.out.println("O cep não corresponde com as regras de negocio ");
+        }
+    }
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+            throw new CepInvalidoException();
+
+            return "23.765-064";
+    }
+}
+
+class CepInvalidoException extends Exception {
+    public CepInvalidoException() {
+        super("CEP inválido.");
+    }
+}
+
+
+
